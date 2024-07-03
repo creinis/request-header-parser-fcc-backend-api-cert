dcb3101d246b4c47db0077cb32eddd8824f3bf8d
creinis <115181527+creinis@users.noreply.github.com>
2024-07-03 14:24:00 +0200

    schema props update

--- models/Request.js
+++ models/Request.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 const requestSchema = new mongoose.Schema({
-  ipaddress: String,
-  language: String,
-  software: String,
-  timestamp: { type: Date, default: Date.now }
+  ipaddress: { type: String, required: true },
+  language: { type: String, required: true },
+  software: { type: String, required: true },
+  timestamp: { type: Date, default: Date.now },
 });
 
 const Request = mongoose.model('Request', requestSchema);
